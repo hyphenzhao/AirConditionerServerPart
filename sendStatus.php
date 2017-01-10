04fffb78ae488efad6c285dab7e7338d12e0d112
hyphenzhao <hzha7521@uni.sydney.edu.au>
2017-01-11 01:15:22 +0800

    Modified as customer wished.

--- sendStatus.php
+++ sendStatus.php
@@ -8,5 +8,15 @@
   $connection = new mysqli($host, $username, $password, $database);
   $sql = "INSERT INTO Status VALUES(NULL, {$temp}, {$humi}, FALSE);";
   $connection->query($sql);
-  echo "Connection Succeed!";
+  $sql = "SELECT * FROM CommandList where executed=0;";
+  $result = $connection->query($sql);
+  if($result->num_rows > 0)
+  {
+    $commands = $result->fetch_assoc();
+    $top_command_no = $commands["commandNo"];
+    $top_command_id = $commands["commandID"];
+    echo $top_command_id;
+    $sql = "UPDATE CommandList SET executed=1 WHERE commandNo={$top_command_no};";
+    $connection->query($sql);
+  }
 ?>
